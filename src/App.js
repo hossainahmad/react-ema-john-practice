586695b7118bf47d80baa0f513fbb1031208f747
hossainahmad <hossain.ahmad38@gmail.com>
2022-03-24 19:36:16 +0600

    Components and some resources Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from Ema-John Projects</h1>
+      <Header></Header>
     </div>
   );
 }
